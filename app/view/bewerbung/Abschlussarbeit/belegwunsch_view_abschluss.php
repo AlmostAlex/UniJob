1c31faeb8bd657999fd933b5b3f034a4b9313790
chimidoro <33393205+chimidoro@users.noreply.github.com>
2018-10-21 16:30:09 +0200

    vorkenntnisse

--- app/view/bewerbung/Abschlussarbeit/belegwunsch_view_abschluss.php
+++ app/view/bewerbung/Abschlussarbeit/belegwunsch_view_abschluss.php
@@ -59,9 +59,9 @@
                 </div>
         
                 <div id="v1">
-                
-
+                <i class="fas fa-exclamation-circle custom-tooltip" data-tooltip-custom-classes="tooltip-large tooltip-right tooltip-danger" data-toggle="tooltip" data-placement="right" title="Tooltip on bototm"></i>
                 </div>
+
                 </td>
             </tr>
            <!--  <tr>
